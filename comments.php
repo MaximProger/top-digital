050cfb58b1d3e2c277aca9eef48b11d24665a948
MaximProger <maksimkoshman090@gmail.com>
2022-06-23 12:01:16 +0300

    Внедрил список комментариев

--- comments.php
+++ comments.php
@@ -1,38 +1,74 @@
-<div class="comments my-4">
-    <h3 class="mb-5">Комментарии:</h3>
+<?php
+/**
+ * The template for displaying comments
+ *
+ * This is the template that displays the area of the page that contains both the current comments
+ * and the comment form.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package test-theme
+ */
 
-    <div class="media mb-4">
-        <img src="images/blog/2.jpg" alt="" class="img-fluid d-flex mr-4 rounded">
-        <div class="media-body">
-            <h5>Антон Колесников</h5>
-            <span class="text-muted">20 января 2020</span>
-            <p class="mt-2">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nisi laborum dolores quidem ea optio fuga nesciunt tempora, in tenetur iusto!</p>
+/*
+ * If the current post is protected by a password and
+ * the visitor has not yet entered the password we will
+ * return early without loading the comments.
+ */
+if ( post_password_required() ) {
+    return;
+}
+?>
 
-            <a href="#" class="reply">Ответить <i class="fa fa-reply"></i></a>
+<div id="comments" class="comments my-4">
 
-            <div class="media mt-5">
-                <img src="images/blog/2.jpg" alt="" class="img-fluid d-flex mr-4 rounded">
-                <div class="media-body">
-                    <h5>Егор Савицкий</h5>
-                    <span class="text-muted">20 января 2020</span>
-                    <p class="mt-2">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nisi laborum dolores quidem ea optio fuga nesciunt tempora, in tenetur iusto!</p>
+    <?php
+    // You can start editing here -- including this comment!
+    if ( have_comments() ) :
+        ?>
+        <h3 class="mb-5">Комментарии:</h3>
 
-                    <a href="#" class="reply">Ответить <i class="fa fa-reply"></i></a>
-                </div>
-            </div>
-        </div>
-    </div>
-    <div class="media mb-4">
-        <img src="images/blog/2.jpg" alt="" class="img-fluid d-flex mr-4 rounded">
-        <div class="media-body">
-            <h5>Валентин Крашков</h5>
-            <span class="text-muted">14 февраля 2020</span>
-            <p class="mt-2">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nisi laborum dolores quidem ea optio fuga nesciunt tempora, in tenetur iusto!</p>
+        <?php the_comments_navigation(); ?>
 
-            <a href="#" class="reply">Ответить <i class="fa fa-reply"></i></a>
-        </div>
-    </div>
-</div>
+        <ol class="comment-list">
+            <?php
+            wp_list_comments(
+                array(
+                    'walker'            => new Bootstrap_Walker_Comment(),
+                    'max_depth'         => 2,
+                    'style'             => 'ol',
+                    'callback'          => null,
+                    'end-callback'      => null,
+                    'type'              => 'comment',
+                    'reply_text'        => __('Ответить <i class="fa fa-reply"></i>'),
+                    'per_page'          => 10,
+                    'avatar_size'       => 80,
+                    'reverse_top_level' => true,
+                    'reverse_children'  => true,
+                    'format'            => 'html5', // или xhtml, если HTML5 не поддерживается темой
+                    'short_ping'        => true,    // С версии 3.6,
+                    'echo'              => true,     // true или false
+                )
+            );
+            ?>
+        </ol><!-- .comment-list -->
+
+        <?php
+        the_comments_navigation();
+
+        // If comments are closed and there are comments, let's leave a little note, shall we?
+        if ( ! comments_open() ) :
+            ?>
+            <p class="no-comments"><?php esc_html_e( 'Comments are closed.', 'test-theme' ); ?></p>
+        <?php
+        endif;
+
+    endif; // Check for have_comments().
+
+    comment_form();
+    ?>
+
+</div><!-- #comments -->
 
 <div class="mt-5 mb-3">
     <h3 class="mt-5 mb-2">Оставьте комментарий</h3>
